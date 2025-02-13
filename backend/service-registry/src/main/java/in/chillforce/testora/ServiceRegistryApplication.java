c01d562a7a5b3e9c4787963be8fabbaa6fd64c4c
Ragavan Sellamuthu <ragavansellamuthu.techymeet@gmail.com>
2025-02-13 17:44:30 +0530

    Initial Commit: Service-Registry

--- backend/service-registry/src/main/java/in/chillforce/testora/ServiceRegistryApplication.java
+++ backend/service-registry/src/main/java/in/chillforce/testora/ServiceRegistryApplication.java
@@ -1,10 +1,8 @@
-package in.chillforce.testora.registry;
+package in.chillforce.testora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
--- backend/service-registry/src/test/java/in/chillforce/testora/ServiceRegistryApplicationTests.java
+++ backend/service-registry/src/test/java/in/chillforce/testora/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package in.chillforce.testora.registry;
+package in.chillforce.testora;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

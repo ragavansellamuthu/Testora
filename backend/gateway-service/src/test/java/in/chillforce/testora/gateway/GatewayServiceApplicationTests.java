1b907c43655f3bd2f096ea00b14eca98db916351
Ragavan Sellamuthu <ragavansellamuthu.techymeet@gmail.com>
2025-02-13 17:55:57 +0530

    Initial Commit: Gateway Service

---
+++ backend/gateway-service/src/test/java/in/chillforce/testora/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.chillforce.testora.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b1143ced68d0937fdd31dc73ea342684c80bd176
Ragavan Sellamuthu <ragavansellamuthu.techymeet@gmail.com>
2025-02-13 17:39:19 +0530

    Initial Commit: Service Registry

---
+++ service-registry/src/test/java/in/chillforce/testora/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package in.chillforce.testora.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
